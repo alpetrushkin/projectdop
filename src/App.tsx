562d10cf3453f0e05953276ac6321844cc77d1b5
alpetrushkin <alpetrushkin1984@mail.ru>
2022-07-20 07:37:32 +0300

    метод map домашнее задание

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,34 @@ import {NewComponent} from "./NewComponent";
 
 function App() {
 
-    let [students, setStudents] = useState([
-        {id: 1, name: 'Alex', age: 23},
-        {id: 2, name: 'Dima', age: 18},
-        {id: 3, name: 'Denis', age: 38},
-        {id: 4, name: 'Sveta', age: 17},
-        {id: 5, name: 'Ivan', age: 20},
-        {id: 6, name: 'Olga', age: 24},
-        {id: 7, name: 'Valera', age: 33},
-        {id: 1, name: 'Alex', age: 23},
-        {id: 2, name: 'Dima', age: 18},
-        {id: 3, name: 'Denis', age: 38},
-        {id: 4, name: 'Sveta', age: 17},
-        {id: 5, name: 'Ivan', age: 20},
-        {id: 6, name: 'Olga', age: 24},
-        {id: 7, name: 'Valera', age: 33},
-    ])
+    // let [students, setStudents] = useState([
+    //     {id: 1, name: 'Alex', age: 23},
+    //     {id: 2, name: 'Dima', age: 18},
+    //     {id: 3, name: 'Denis', age: 38},
+    //     {id: 4, name: 'Sveta', age: 17},
+    //     {id: 5, name: 'Ivan', age: 20},
+    //     {id: 6, name: 'Olga', age: 24},
+    //     {id: 7, name: 'Valera', age: 33},
+    //     {id: 1, name: 'Alex', age: 23},
+    //     {id: 2, name: 'Dima', age: 18},
+    //     {id: 3, name: 'Denis', age: 38},
+    //     {id: 4, name: 'Sveta', age: 17},
+    //     {id: 5, name: 'Ivan', age: 20},
+    //     {id: 6, name: 'Olga', age: 24},
+    //     {id: 7, name: 'Valera', age: 33},
+    // ])
+    const topCars = [
+        {manufacturer: 'Suzuki', model: 'vitara'},
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'},
+        {manufacturer: 'Lada', model: 'vesta'},
+    ]
 
     return (
         <div className="App">
-            <NewComponent students={students}/>
+            {/*<NewComponent students={students}/>*/}
+            <NewComponent topCars={topCars}/>
         </div>
     );
 }
--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,30 +1,60 @@
 import React from 'react';
 
-type NewComponentType = {
-    // students:Array<StudentType>
-    students: StudentType[]
+/*type NewComponentType = {
+   // students:Array<StudentType>
+   students: StudentType[]
 }
 
 type StudentType = {
-    id: number,
-    name: string,
-    age: number
+   id: number,
+   name: string,
+   age: number
+}*/
+
+/*export const NewComponent = (props: NewComponentType) => {
+   const topCars = [
+      {manufacturer: 'BMW', model: 'm5cs'},
+      {manufacturer: 'Mercedes', model: 'e63s'},
+      {manufacturer: 'Audi', model: 'rs6'},
+   ]
+   return (
+      <div>
+         <ul>
+            {props.students.map((objectFromStudentArray, index) => {
+               return (
+                  <li key={index}>
+                     <span>{objectFromStudentArray.name}</span>
+                     <span> age: {objectFromStudentArray.age}</span>
+                  </li>
+               )
+            })}
+         </ul>
+      </div>
+   );*/
+// };
+
+type NewComponentType = {
+   topCars: Array<topCarsArray>
+}
+
+type topCarsArray = {
+   manufacturer: string,
+   model: string
 }
 
 export const NewComponent = (props: NewComponentType) => {
-    return (
-        <div>
-            <ul>
-                {props.students.map((objectFromStudentArray, index) => {
-                    return (
-                        <li key={index}>
-                            <span>{objectFromStudentArray.name}</span>
-                            <span> age: {objectFromStudentArray.age}</span>
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    );
-};
+   return (
+      <table>
 
+         {props.topCars.map((objTopCars, index) => {
+            debugger
+            return (
+               <tr key={index}>
+                  <th>{objTopCars.manufacturer}</th>
+                  <th>{objTopCars.model}</th>
+               </tr>
+            )
+         })}
+      </table>
+   )
+}
